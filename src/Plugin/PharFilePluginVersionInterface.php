cab8402279487efa2e6b760e2c3e1ac334c5cf0f
David Molineus <david.molineus@netzmacht.de>
2022-02-08 08:41:55 +0100

    Add doc

--- src/Plugin/PharFilePluginVersionInterface.php
+++ src/Plugin/PharFilePluginVersionInterface.php
@@ -9,6 +9,8 @@ interface PharFilePluginVersionInterface extends PluginVersionInterface
     /**
      * Gets the path to the plugin.php inside the phar file, e.g. plugin.php
      *
+     * The file itself must instantiate an instance of the plugin bootstrap itself and has to return it.
+     *
      * @return string
      */
     public function getPluginPath(): string;
