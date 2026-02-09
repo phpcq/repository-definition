954f9b94254c24518216accc10d5fb0b03139c47
David Molineus <david.molineus@netzmacht.de>
2026-02-09 14:06:51 +0100

    Clean up rector.php

--- rector.php
+++ rector.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
-use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 use Rector\Privatization\Rector\Class_\FinalizeTestCaseClassRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\TypeDeclaration\Rector\Class_\AddTestsVoidReturnTypeWhereNoReturnRector;
@@ -21,14 +19,4 @@ return RectorConfig::configure()
     ])
     ->withSets([
         LevelSetList::UP_TO_PHP_81,
-    ])
-    ->withSkip([
-        __DIR__ . '/src/Platform/PlatformInformation.php',
-        ClosureToArrowFunctionRector::class => [
-            __DIR__ . '/src/Report/Writer/DiagnosticIterator.php'
-        ],
-        ChangeSwitchToMatchRector::class => [
-            // Better reflection does not work with a constant used here
-            __DIR__ . '/tests/Platform/PlatformInformationTest.php'
-        ]
     ]);
